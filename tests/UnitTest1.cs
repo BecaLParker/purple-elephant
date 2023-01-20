06fb791569ba52eb46459e5ea83b652166773c82
BecaLParker <beca.parker@red-gate.com>
2023-01-20 16:51:41 +0000

    test for a known value in testDB

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -1,28 +1,38 @@
 using Npgsql;
+using FluentAssertions;
 
 namespace tests;
 
 public class Tests
 {
+    private const string _testDataBaseName = "music_library_test";
+    private static readonly string? _serverConnString = Environment.GetEnvironmentVariable("PostgreSQLConnectionString");
+    readonly string _connString = _serverConnString + "Database=" + _testDataBaseName;
+    
     [SetUp]
-    public async Task Setup()
+    public void Setup()
     {
-        const string testDataBaseName = "music_library_test";
-        string? serverConnString = Environment.GetEnvironmentVariable("PostgreSQLConnectionString");
-        if (serverConnString is null)
+        if (_serverConnString is null)
         {
             throw new ArgumentException(
                 "Connection string not found. Store connection string in environment variable PostgreSQLConnectionString.");
         }
-
-        string connString = serverConnString + "Database=" + testDataBaseName;
-        await using var conn = new NpgsqlConnection(connString);
-        await conn.OpenAsync();
     }
 
     [Test]
-    public void Test1()
+    public async Task testDB_CorrectlySeeded_ShouldContainDoolittleAlbum()
     {
-        Assert.Pass();
+
+        await using var conn = new NpgsqlConnection(_connString);
+        await conn.OpenAsync();
+        
+        // Query for a known value
+        await using var cmd = new NpgsqlCommand("SELECT title FROM albums WHERE id = 1", conn);
+        await using var reader = await cmd.ExecuteReaderAsync();
+        while (await reader.ReadAsync())
+        {
+            var result = reader.GetString(0);
+            result.Should().Be("Doolittle", "that's how I seeded the testDB");
+        }
     }
 }
